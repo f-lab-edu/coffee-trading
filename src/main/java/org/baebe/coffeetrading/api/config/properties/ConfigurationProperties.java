4a6155b898da4b7f2af8e5335d14ff8d8346408f
bae-be <zpks1128@naver.com>
2025-03-18 16:02:27 +0900

    chore : 미사용 Cors 설정 주석 활성화

--- src/main/java/org/baebe/coffeetrading/api/config/properties/ConfigurationProperties.java
+++ src/main/java/org/baebe/coffeetrading/api/config/properties/ConfigurationProperties.java
@@ -3,14 +3,10 @@ package org.baebe.coffeetrading.api.config.properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Properties의 값을 불러와야하는 클래스를 정의한다.
- */
-
 @Configuration
 @EnableConfigurationProperties({
-    JwtProperties.class,
-    CorsProperties.class
+    JwtProperties.class
+//    CorsProperties.class
 })
 public class ConfigurationProperties {
 
