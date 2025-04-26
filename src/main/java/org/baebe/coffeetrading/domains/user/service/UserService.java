58baccb92205c32c81fd167b660666a34c93fa7a
bae-be <zpks1128@naver.com>
2025-04-27 06:58:33 +0900

    refactor :    - 클래스 레벨에 transactional 설정

--- src/main/java/org/baebe/coffeetrading/domains/user/service/UserService.java
+++ src/main/java/org/baebe/coffeetrading/domains/user/service/UserService.java
@@ -13,39 +13,34 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final UsersRepository usersRepository;
 
-    @Transactional(readOnly = true)
     public UsersEntity getByUserId(Long userId) {
         return usersRepository.findById(userId)
             .orElseThrow(() -> new CoreException(ErrorTypes.USER_NOT_FOUND));
     }
 
-    @Transactional(readOnly = true)
     public UsersEntity getByUserEmail(String email) {
         return usersRepository.findByEmail(email)
             .orElseThrow(() -> new CoreException(ErrorTypes.USER_NOT_FOUND));
     }
 
-    @Transactional(readOnly = true)
     public Optional<UsersEntity> nullableGetByUserEmail(String email) {
         return usersRepository.findByEmail(email);
     }
 
-    @Transactional(readOnly = true)
     public boolean existsByEmail(String email) {
         return usersRepository.existsByEmailEqualsIgnoreCase(email);
     }
 
-    @Transactional(readOnly = true)
     public boolean existsByNickname(String nickname) {
         return usersRepository.existsByNickname(nickname);
     }
 
-    @Transactional(readOnly = true)
     public List<UsersEntity> getByUsernameAndBirthdayAndPhone(String userName, String birthDay, String phone) {
         return usersRepository.findByUserNameAndBirthDayAndPhone(userName, birthDay, phone);
     }
