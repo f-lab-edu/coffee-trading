e8eb74462aca8b666f2d4d31378a5659436b4714
bae-be <zpks1128@naver.com>
2025-06-08 11:11:02 +0900

    refactor :    - cors 설정 변경

--- src/main/java/org/baebe/coffeetrading/api/config/security/CorsConfig.java
+++ src/main/java/org/baebe/coffeetrading/api/config/security/CorsConfig.java
@@ -1,33 +1,33 @@
-//package org.baebe.coffeetrading.api.config.security;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.baebe.coffeetrading.api.config.properties.CorsProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//@Slf4j
-//@Configuration
-//@RequiredArgsConstructor
-//public class CorsConfig {
-//
-//    private final CorsProperties corsProperties;
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        corsProperties.getOrigins().forEach(origin -> log.debug("Allowed Cors Origins >>> {}", origin));
-//
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.setAllowedOriginPatterns(corsProperties.getOrigins());
-//        config.setAllowedMethods(corsProperties.getAllowedMethods());
-//        config.addAllowedHeader(CorsConfiguration.ALL);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//    }
-//}
+package org.baebe.coffeetrading.api.config.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.baebe.coffeetrading.api.config.properties.CorsProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+    private final CorsProperties corsProperties;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        corsProperties.getOrigins().forEach(origin -> log.debug("Allowed Cors Origins >>> {}", origin));
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(corsProperties.getOrigins());
+        config.setAllowedMethods(corsProperties.getAllowedMethods());
+        config.addAllowedHeader(CorsConfiguration.ALL);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
